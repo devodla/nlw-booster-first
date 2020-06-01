dde5174e1ee768153d966616720163d621ec0232
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-06-01 20:32:31 -0300

    feat(server) moving for one folder

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+  console.log('Listagem de usuários');
+
+  // JSON 
+
+  response.json([
+    'Diego',
+    'Cleiton',
+    'Robson',
+    'Daniel'
+  ]);
+});
+
+app.listen(3333);
