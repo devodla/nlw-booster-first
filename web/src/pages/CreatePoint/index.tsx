e3b117d4470966a547f6e158ef7d92e344b2e57f
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-06-06 20:33:54 -0300

    feat(point) add point in Map

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 
@@ -59,6 +60,14 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço no mapa</span>
           </legend>
+
+          <Map center={[ -23.5972961, -46.5836981 ]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[ -23.5972961, -46.5836981 ]} />
+          </Map>
 
           <div className="field-group">
             <div className="field">
