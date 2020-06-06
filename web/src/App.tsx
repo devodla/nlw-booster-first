a0c0862c57ed2c9f66ede57f916fcc7f781aa9b3
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-06-06 18:55:35 -0300

    feat(home) add icons

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Wolrd</h1>
-      </header>
-    </div>
+    <Home />
   );
 }
 
