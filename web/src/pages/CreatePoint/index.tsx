7b4bb2f8c8bc7c25e2e0416dac796535a9e757d8
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-06-07 00:15:06 -0300

    feat(web) add history for main return

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react'; 
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import axios from 'axios';
@@ -39,6 +39,8 @@ const CreatePoint = () => {
   const [ selectedCity, setSelectedCity ] = useState('0');
   const [ selectedItems, setSelectedItems ] = useState<number[]>([]);
   const [ selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+
+  const history = useHistory();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
@@ -131,6 +133,8 @@ const CreatePoint = () => {
     await api.post('points', data);
 
     alert('Ponto de coleta criado!');
+
+    history.push('/');
   }
 
   return (
